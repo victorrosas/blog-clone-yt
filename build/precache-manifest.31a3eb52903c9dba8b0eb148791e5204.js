32c4ee024df6b419b896e570c69df726e2e804c5
Victor Rosas <victor.rosas.96@hotmail.com>
2020-10-26 17:34:52 -0300

    build

---
+++ build/precache-manifest.31a3eb52903c9dba8b0eb148791e5204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d21012746c7dd37910c24d72aa2febd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fb24f729b7a7bd8c6ac",
+    "url": "/static/css/main.22043f62.chunk.css"
+  },
+  {
+    "revision": "c971bc547d8c89413d5e",
+    "url": "/static/js/2.feb0be45.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.feb0be45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb24f729b7a7bd8c6ac",
+    "url": "/static/js/main.6f39c8ea.chunk.js"
+  },
+  {
+    "revision": "9cf1ae8e70b01b27d494",
+    "url": "/static/js/runtime-main.7e871748.js"
+  }
+]);
